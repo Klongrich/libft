78ca6a98c7c13790f53026526a19ef517e768301
Kyle LONGRICH <klongric@e1z2r6p11.42.us.org>
2017-01-27 17:27:25 -0800

    Here it is

---
+++ ft_memcpy.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klongric <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/06 12:34:25 by klongric          #+#    #+#             */
+/*   Updated: 2017/01/18 23:51:12 by klongric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	void	*ret;
+
+	ret = dst;
+	while (n--)
+		*(unsigned char *)dst++ = *(unsigned char*)src++;
+	return (ret);
+}
